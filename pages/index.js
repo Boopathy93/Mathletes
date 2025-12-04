2af4b5272a7a2768effb07d23639f9f925ea4d2d
Boopathy93 <flowermath93@gmail.com>
2025-12-04 16:15:44 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      {/* Your existing index.html content should go here */}
+      {/* For now, this file just needs to exist for Next.js to work */}
+      <h1>Mathletes Academy</h1>
+    </div>
+  );
+}
